33f95c21a7bf3dc778fa15304ca9b76cd0c39fa7
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-12-10 13:00:39 -0500

    intro: TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spotify';
+
+
+
+  car: CarModel = {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 2022,
+  }
+
+  listCars: Array< CarModel> = [
+    {brand: 'Ford', model: 'Mustang', year: 2022},
+    {brand: 'Tesla ', model: 'Tesla ', year: 2020},
+    {brand: 'Toyota ', model: 'Corolla ', year: 2021},
+  ]
+
+  // name: string = 'Ever'
+  // age: number = 23
+  // phone: null =null
+  // email: undefined = undefined
+  // datum: any = () => {}
+  // status: boolean = true
+
+
+
+}
+
+interface CarModel {
+  brand: string;
+  model: string;
+  year?: number;
 }
