04906b7f2379671ec0611d1463968b8abd51bd7c
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-12-10 21:31:34 -0500

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
 
 const routes: Routes = [
-  {
-    path: '', //? --> /
-    component: ExampleComponent
-  }
+  {}
 ];
 
 @NgModule({
