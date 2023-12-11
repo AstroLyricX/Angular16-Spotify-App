6f0d3b3b0b91a35c4f9d0682ccff1b76a2631b09
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-12-10 21:35:28 -0500

    update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
-@NgModule({
+@NgModule({ //? Declaraciones, componentes, directivas, pipes
   declarations: [
     AppComponent,
   ],
-  imports: [
+  imports: [ //? Solo se importan otros modules
     BrowserModule,
     AppRoutingModule
   ],
