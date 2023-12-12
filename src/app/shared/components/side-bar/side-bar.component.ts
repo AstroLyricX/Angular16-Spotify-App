6c708be0aabb482c416a723e0dd8435246cb6386
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-12-12 04:29:21 -0500

    update side-bar.component.ts

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
