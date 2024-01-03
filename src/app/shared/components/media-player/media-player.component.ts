adbb0f390eb9b970029e9127e3696cddd5188af6
Ever Ronaldo <ever.condori@upeu.edu.pe>
2024-01-03 05:04:59 -0500

    update media-player.component.css, media-player.component.html and media-player.component.ts

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-media-player',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit {
+
+  mockCover: any = {
+    cover: 'https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png',
+    album: 'Gioli & Assi',
+    name: 'BEBE (Oficial)',
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
