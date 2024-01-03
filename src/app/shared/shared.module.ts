e28d88097ed68f4dd5c8ee7301f89cb7134d72d0
Ever Ronaldo <ever.condori@upeu.edu.pe>
2024-01-03 05:04:53 -0500

    update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,12 +8,17 @@ import { HeaderUserComponent } from './components/header-user/header-user.compon
 
 @NgModule({
   declarations: [
-    MediaPlayerComponent,
+    // MediaPlayerComponent,
     HeaderUserComponent
   ],
   imports: [
     CommonModule
   ],
+  exports: [
+    // SideBarComponent,
+    // MediaPlayerComponent,
+    HeaderUserComponent,
+  ]
 
 })
 export class SharedModule { }
