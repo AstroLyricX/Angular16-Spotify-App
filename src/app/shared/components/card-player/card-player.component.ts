62aa7b9ec47619049e8a1a82f363e9c24166494d
Ever Ronaldo <ever.condori@upeu.edu.pe>
2024-01-14 14:43:40 -0500

    update card-player.component.css, card-player.component.html and card-player.component.ts

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TracksModule } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-card-player',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-player.component.css']
 })
 export class CardPlayerComponent {
+  @Input() mode: 'small' | 'big' = 'small';
+  // @Input() track!: TracksModule;
+  @Input() track: TracksModule = {_id: 0, name: '', album: '', cover: '', url: ''};
+
 
 }
